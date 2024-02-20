d2f4d60097bd1cf50055c1023ff28b5eb0292985
Hieu Nguyen <hieunp@batgroup.vn>
2024-02-20 11:51:42 +0700

    feat: add demo flow login oauth2.0 google

---
+++ src/Login.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+
+export default function Login() {
+  const [params] = useSearchParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const access_token = params.get("access_token");
+    const refresh_token = params.get("refresh_token");
+    const new_user = params.get("new_user");
+    const verify = params.get("verify");
+
+    // Dựa vào new_user, verify để biết là user mới hay user cũ và đã  verify email hay chưa
+    localStorage.setItem("access_token", access_token);
+    localStorage.setItem("refresh_token", refresh_token);
+    navigate("/");
+  }, [params, navigate]);
+  return <div>Login</div>;
+}
